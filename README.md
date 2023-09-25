60dd3d2ea276605e22c67b2fe0d40219e5316ec0
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-09-25 19:41:36 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Biblioteca react-router-dom
+
+## Para Instalar
+```
+npm i react-router-dom
+```
+
+### BrowserRouter:
+Define onde a área do nosso app que vai trocar as páginas.
+
+### Routes:
+Define as rotas
+
+### Route:
+Um elemento deste para cada rota, configurar com path e componente da rota.
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <div>React Router</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
