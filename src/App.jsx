b260820e14c0cf48195071bfb67a7cb7eb3f0688
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-09-26 20:21:45 -0300

    Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Navbar from './components/Navbar'
 import Product from './pages/Product'
 import Info from './pages/Info'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <p>Page not Found</p>
+      <p>Tururúúúú tu ru rúúú rúúúú</p>
+    </>
+  )
+}
+
+export default NotFound
