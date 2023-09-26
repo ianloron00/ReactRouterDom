45d41dfdbd753da8f649e539d26d78ae5c2f7641
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-09-26 20:04:29 -0300

    Dynamic Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Navbar from './components/Navbar'
+import Product from './pages/Product'
 function App() {
   return (
     <div className="App">
       <div>React Router</div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Product.jsx
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom"
+import { useFetch } from '../hooks/useFetch'
+
+const Product = () => {
+  // Rota Dinâmica
+  const { id } = useParams()
+  const url = "http://localhost:3000/products/" + id
+  const { data: product, loading, error }= useFetch(url)
+
+  return (
+    <>
+      <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{ product.name }</h1>
+          <p>R${ product.price }</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Product
