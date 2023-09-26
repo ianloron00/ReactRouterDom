1d2574f9ddf56b62567c6d4e39b89be78ad7b994
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-09-26 20:54:47 -0300

    Search Params

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// components
+import Navbar from './components/Navbar'
+import SearchForm from './components/SearchForm'
+
+// pages
 import Home from './pages/Home'
 import About from './pages/About'
-import Navbar from './components/Navbar'
 import Product from './pages/Product'
 import Info from './pages/Info'
 import NotFound from './pages/NotFound'
+import Search from './pages/Search'
 
 function App() {
   return (
@@ -13,11 +19,13 @@ function App() {
       <div>React Router</div>
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="/search" element={<Search />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
